f26d7aa4d1b3509ee15c76764009c7d6f144535d
Ashok Nayak <ashok.nayakii@gmail.com>
2022-01-14 15:34:16 -0500

    added task type inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,23 @@ var createTaskHandler = function(event) {
 
   event.preventDefault();
 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "text-input";
+
+    // create div to hold task info and add to list item 
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className= "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
+
+    console.dir(listItemEl);
 };
 
   formEl.addEventListener("submit", createTaskHandler);
